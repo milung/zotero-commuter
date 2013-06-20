4fd190c4b8edf5d227341cd96083b589c411b845
milan_kvetko <milan_kvetko@localhost>
2013-06-20 15:16:13 +0200

    * fixed vcs structure

---
+++ ZoteroSentience/src/sk/mung/sentience/zoteroapi/entities/Item.java
@@ -0,0 +1,54 @@
+package sk.mung.sentience.zoteroapi.entities;
+
+import java.util.List;
+
+
+public interface Item extends KeyEntity
+{
+
+    ItemType getItemType();
+
+    void setItemType(ItemType itemType);
+
+    String getKey();
+
+    void setKey(String key);
+
+    int getVersion();
+
+    void setVersion(int version);
+
+    String getTitle();
+
+    void setTitle(String title);
+
+    List<Field> getFields();
+
+    ItemField[] getSupportedFields();
+
+    void addField(Field field);
+
+    boolean isSynced();
+
+    void setSynced(boolean isSynced);
+
+    List<Creator> getCreators();
+
+    void addCreator(Creator creator);
+
+    List<CollectionEntity> getCollections();
+
+    void addCollection(CollectionEntity col);
+
+    List<Tag> getTags();
+
+    void addTag(Tag tag);
+
+    String getParentKey();
+
+    void setParentKey(String parentKey);
+
+    List<Item> getChildren();
+
+    void addChild(Item item);
+}
